cb361636a0716da9d877fb549f53f4ab3ec4e9af
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-27 11:06:23 +0530

    update layout

--- static/index.js
+++ static/index.js
@@ -34,3 +34,47 @@ function calculateNewTime(time) {
 
     return `${mins.length === 2 ? mins : '0' + mins}:${secs.length === 2 ? secs : '0' + secs}`;
 }
+
+function triggerTimer() {
+    const timers = document.querySelectorAll('.timer');
+
+    timers.forEach(timer => {
+        const timerInterval = setInterval(() => {
+            const timeLeft = timer.textContent.trim();
+
+            if (timeLeft === '00:00') {
+                sendOrderNumber({
+                    status: 'DONE',
+                    no: timer.dataset.orderNumber,
+                    time: 0
+                });
+                return clearInterval(timerInterval);
+            }
+            
+            timer.textContent = calculateNewTime(timeLeft);
+        }, 1000);
+    });
+}
+
+function populateOrderNumbers(orderNumbers) {
+    const doneList = document.querySelector('.done-list');
+    doneList.innerHTML = orderNumbers['DONE'].map(orderNumber => `
+        <h2 class="col-md-3 col-xs-3"> 
+            <span class="order-number-item">${orderNumber.no ? orderNumber.no : ' '}</span>
+        </h2>
+    `).join('');
+
+    const preparingList = document.querySelector('.preparing-list');
+    preparingList.innerHTML = orderNumbers['PREPARING'].map(orderNumber => `
+        <h2 class="col-md-3 col-xs-3"> 
+            <span class="order-number-item">
+                ${orderNumber.no ? orderNumber.no : ' '}
+                <span class="order-number-item-time ${orderNumber.no ? 'timer': ''}" data-order-number="${orderNumber.no ? orderNumber.no : ' '}">
+                    ${orderNumber.no ? orderNumber.time : ' '}
+                </span>
+            </span>
+        </h2>
+    `).join('');
+
+    triggerTimer();
+}
